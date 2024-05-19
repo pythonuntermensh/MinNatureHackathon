a9440a72e900bc260fb68feb1f5cda537e643515
optional <pythonuntermensh@gmail.com>
2024-05-19 07:52:33 +0400

    Delete parnocopitny directory

--- parnocopitny/components/classified-files.tsx
+++
@@ -1,9 +0,0 @@
-const ClassifiedFiles = () => { 
-  return (
-    <div className="flex flex-col w-full">
-      <p className="text-center text-lg font-semibold p-3">Результат классификации</p>
-    </div>
-  )
-}
-
-export default ClassifiedFiles;
--- parnocopitny/http/classifyArchive.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
-
-export const classifyArchive = async (formData: FormData) => {
-  const response = axios.post("http://localhost:8000/classify", formData, {responseType: "arraybuffer", headers: {'Access-Control-Expose-Headers': 'Content-Disposition'}});
-  return response;
-}
